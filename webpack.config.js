b5b85be47bba9205db0cef5759839e82111e8ace
Yahor Dubeshka <y.dubeshka@smartpro.by>
2023-07-28 14:34:45 +0300

    Loading fonts

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
                 test: /\.(png|svg|jpe?g|gif)$/i,
                 type: 'asset/resource',
             },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            }
         ],
     },
 };
