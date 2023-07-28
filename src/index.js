458868f1ced4193c19443dd586b6ae5e5d1c3d05
Yahor Dubeshka <y.dubeshka@smartpro.by>
2023-07-28 15:02:04 +0300

    Loading data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png';
+import Icon from './assets//icons/icon.png';
+import Data from './assets//tables/data.xml';
+import Notes from './assets//tables/data.csv';
+import toml from './assets//tables/data.toml';
+import yaml from './assets//tables/data.yaml';
+import json from './assets//tables/data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
  function component() {
     const element = document.createElement('div');  
@@ -13,8 +27,10 @@ import Icon from './icon.png';
     const myIcon = new Image();
     myIcon.src = Icon;
 
-  element.appendChild(myIcon);
+    element.appendChild(myIcon);
 
+    console.log(Data);
+    console.log(Notes);
 
    return element;
  }
